83e2ec44f2d32fae81b47f2c7e9e10e587567d27
ViRALTek <99914381+ViRALTek@users.noreply.github.com>
2022-02-17 20:46:57 +0100

    NeoOS 17022022 Test
    This is a beta test version. Errors may accour.
    It is not developed much.

---
+++ NeoOS 17022022 Test.bat
@@ -0,0 +1,7 @@
+@echo off
+echo NeoOS 0.0.1
+echo Hi!
+echo This is a Test NeoOS Ultraalpha
+pause
+x=msgbox("NeoOS is not done further.You can download new versions on github." , 2 =48, "NeoOS 0.0.1")
+pause
